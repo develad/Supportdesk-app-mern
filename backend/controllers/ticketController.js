1fd29f109ff9f9e61b7726442e1d5b8ce2143f03
develad <develadb@gmail.com>
2023-03-14 17:59:28 +0200

    Single Ticket, Update & Delete (Backend) - Chapter 137

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -23,6 +23,38 @@ const getTickets = asyncHandler(async (req, res) => {
   res.status(200).json(tickets);
 });
 
+// @desc    Get user ticket
+// @route   GET /api/tickets/:id
+// @access  Private
+
+const getTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  // When validating with the token we setting the req.user.id through the authMiddleware
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    // 401 -> unauthorized
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  // Limit only to specific logged in user to get his ticket
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  res.status(200).json(ticket);
+});
+
 // @desc    Create new ticket
 // @route   POST /api/tickets
 // @access  Private
@@ -54,7 +86,82 @@ const createTicket = asyncHandler(async (req, res) => {
   res.status(201).json(ticket);
 });
 
+// @desc    Delete  ticket
+// @route   DELETE /api/tickets/:id
+// @access  Private
+
+const deleteTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  // When validating with the token we setting the req.user.id through the authMiddleware
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    // 401 -> unauthorized
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  // Limit only to specific logged in user to get his ticket
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+// @desc    Update user ticket
+// @route   PUT /api/tickets/:id
+// @access  Private
+
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  // When validating with the token we setting the req.user.id through the authMiddleware
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    // 401 -> unauthorized
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  // Limit only to specific logged in user to get his ticket
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("Not Authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    // An option object, {new:true} -> if ticket not already there then create it
+    { new: true },
+  );
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
+  getTicket,
   createTicket,
+  updateTicket,
+  deleteTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,10 +3,21 @@ const router = express.Router();
 
 const { protect } = require("../middleware/authMiddleware");
 
-const { getTickets, createTicket } = require("../controllers/ticketController");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  updateTicket,
+  deleteTicket,
+} = require("../controllers/ticketController");
 
 // With router.route we can chain different Methods
 // With the "protect" we get only the user tickets when he is authenticated
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
