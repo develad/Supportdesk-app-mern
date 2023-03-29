6ee1eb47e1b2cf1c7674e3be1f1beeb08ae400ae
develad <develadb@gmail.com>
2023-03-29 13:54:17 +0300

    Fetch Notes Through Redux - Chapter 147

---
+++ frontend/src/features/notes/noteSlice.js
@@ -0,0 +1,63 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import noteService from "./noteService";
+
+const initialState = {
+  notes: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+// Get ticket notes
+export const getNotes = createAsyncThunk(
+  "notes/getAll",
+  async (ticketId, thunkAPI) => {
+    try {
+      // With the thunkAPI we have access to all of the state in the different slices
+      const token = thunkAPI.getState().auth.user.token;
+      return await noteService.getNotes(ticketId, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  },
+);
+
+export const noteSlice = createSlice({
+  name: "note",
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (bulider) => {
+    bulider
+      .addCase(getNotes.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getNotes.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.notes = action.payload;
+      })
+      .addCase(getNotes.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        // The message is coming from the return thunkAPI.rejectWithValue(message) in the catch clause
+        state.message = action.payload;
+      });
+  },
+});
+
+// Exporting a function outside the reducers in the createslice is from the actions -> noteSlice.actions
+export const { reset } = noteSlice.actions;
+
+// Exporting as default the Reducer to the main store file
+export default noteSlice.reducer;
